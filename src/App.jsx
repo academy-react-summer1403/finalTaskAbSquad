5226db8e093e90f9a42478c9677e3aa27d43aa49
loonydim <bbk97xx@gmail.com>
2024-09-28 10:42:38 +0330

    tailwindConfingColors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="text-primaryBlue">hi this is a test</div>
+      <div className="text-primaryWhite">hi this is a test</div>
+      <div className="text-primaryViolet">hi this is a test</div>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primaryBlue: "#3772FF",
+      primaryWhite: "#FEFDFF",
+      primaryViolet: "#DE59FF",
+    },
     extend: {},
   },
   plugins: [],
