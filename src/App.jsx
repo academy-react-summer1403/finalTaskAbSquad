59160ea38a3a33cf9d97219f3c8584100b8f07b9
loonydim <bbk97xx@gmail.com>
2024-09-28 10:56:07 +0330

    added react-hot-toast changed config for tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
+  toast("hello world");
   return (
     <>
-      <div className="text-primaryBlue">hi this is a test</div>
-      <div className="text-primaryWhite">hi this is a test</div>
-      <div className="text-primaryViolet">hi this is a test</div>
+      <div>
+        <Toaster />
+      </div>
+      <div className="text-primaryBlue">hi this is a test 1</div>
+      <div className="text-primaryWhite">hi this is a test 2</div>
+      <div className="text-primaryViolet">hi this is a test 3</div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primaryBlue: "#3772FF",
-      primaryWhite: "#FEFDFF",
-      primaryViolet: "#DE59FF",
+    extend: {
+      colors: {
+        primaryBlue: "#3772FF",
+        primaryWhite: "#FEFDFF",
+        primaryViolet: "#DE59FF",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
