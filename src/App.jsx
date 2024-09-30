6669221ee7d727b96659347c384344ccf766e931
loonydim <bbk97xx@gmail.com>
2024-09-30 16:02:20 +0330

    RangeSlider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import * as React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-import TextField from "./components/utilities/Fields/TextField";
+import RangeSliderField from "./components/utilities/Fields/RangeSliderField";
+
 function App() {
   return (
     <>
       <Header />
+      <RangeSliderField />
       <Outlet />
       <Footer />
     </>
