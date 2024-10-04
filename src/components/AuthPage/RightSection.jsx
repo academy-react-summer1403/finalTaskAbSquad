7675088b68e1deb98fd97250e50c65a3d959d85f
loonydim <bbk97xx@gmail.com>
2024-10-04 22:00:58 +0330

    leftsecAdded some changes in right

--- src/components/AuthPage/RightSection.jsx
+++ src/components/AuthPage/RightSection.jsx
@@ -2,27 +2,25 @@ import React from "react";
 import BahrLogoComp from "../Common/BahrLogoComp";
 import { FaCocktail } from "react-icons/fa";
 import RightSecRow from "./RightSecRow";
-const RightSection = () => {
+const RightSection = ({
+  Icon,
+  desc,
+  Icon2 = "",
+  desc2 = "",
+  Icon3 = "",
+  desc3 = "",
+  show,
+  show2 = {},
+  show3 = {},
+}) => {
   return (
     <div className="bg-primaryGray w-1/3 p-24 h-screen ">
       <div className=" flex flex-row justify-start items-center max-lg:grow mb-16">
         <BahrLogoComp />
       </div>
-      <RightSecRow
-        className="m-auto"
-        desc="وارد کردن شماره همراه"
-        Icon={FaCocktail}
-      />{" "}
-      <RightSecRow
-        className="m-auto"
-        desc="وارد کردن شماره همراه"
-        Icon={FaCocktail}
-      />{" "}
-      <RightSecRow
-        className="m-auto"
-        desc="وارد کردن شماره همراه"
-        Icon={FaCocktail}
-      />
+      {show && <RightSecRow className="m-auto" desc={desc} Icon={Icon} />}
+      {show2 && <RightSecRow className="m-auto" desc={desc2} Icon={Icon2} />}
+      {show3 && <RightSecRow className="m-auto" desc={desc3} Icon={Icon3} />}
     </div>
   );
 };
