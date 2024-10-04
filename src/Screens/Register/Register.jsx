317fe3a504ad4a4579cdcbf802ac63a75068c71b
loonydim <bbk97xx@gmail.com>
2024-10-04 20:29:21 +0330

    aiden did this

--- src/Screens/Register/Register.jsx
+++ src/Screens/Register/Register.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import RightSection from "../../components/AuthPage/RightSection";
+import LeftSection from "../../components/AuthPage/LeftSection";
 
 const Register = () => {
-  return <RightSection />;
+  return (
+    <div className="flex">
+      <RightSection /> <LeftSection />
+    </div>
+  );
 };
 
 export default Register;
---
+++ src/components/AuthPage/LeftSection.jsx
@@ -0,0 +1,15 @@
+import Button from "../Common/Button/Button";
+import React from "react";
+
+const LeftSection = () => {
+  return (
+    <div>
+      <h2>hi</h2>
+      <p></p>
+      <Button phoneStyle="h-9 w-" text="ورود یا ثبت نام" />
+      <span></span>
+    </div>
+  );
+};
+
+export default LeftSection;
