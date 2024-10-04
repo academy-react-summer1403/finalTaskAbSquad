4afa8847c19e97dd691e16899197d689f3399e90
Aiden <amir138204@yahoo.com>
2024-10-04 19:22:02 +0330

    Made Error Page Responsive

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -8,21 +8,21 @@ const ErrorNotFound = () => {
     // Error Box Container
     <div className="dark:bg-primaryBlack h-screen">
       {/* Error Box */}
-      <div className=" flex flex-col items-center text-center pt-40 relative">
-        <h1 className="text-8xl text-primaryBlue w-content">404</h1>
-        <h2 className="text-primaryBlue text-2xl w-80">
+      <div className=" flex flex-col items-center text-center pt-40 relative lg:w-full lg:mx-0 mx-auto w-80">
+        <h1 className="lg:text-8xl text-7xl text-primaryBlue w-content">404</h1>
+        <h2 className="text-primaryBlue lg:text-2xl  text-base lg:w-80 w-64">
           ما صفحه‌ای که دنبالش هستی رو نتونستیم پیدا کنیم!
         </h2>
         <NavLink to="/">
           <Button text="صفحه اصلی" phoneStyle="my-10 py-[14px] px-6" />
         </NavLink>
         <img
-          className="absolute  w-20 left-[600px] top-32"
+          className="absolute lg:w-20 lg:left-[600px] lg:top-32 top-36 -left-10 w-12"
           src={LeftGlass}
           alt=""
         />
         <img
-          className="absolute  w-20 right-[580px]  bottom-0"
+          className="absolute lg:w-20 lg:right-[580px] lg:bottom-0 w-16 -right-10 bottom-8"
           src={RightGlass}
           alt=""
         />
