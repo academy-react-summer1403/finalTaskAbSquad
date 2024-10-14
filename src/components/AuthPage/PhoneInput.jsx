648184b06b5ec78c2ba4edfb34c189c068e7ff96
loonydim <bbk97xx@gmail.com>
2024-10-14 22:58:54 +0330

    not responsive

--- src/components/AuthPage/PhoneInput.jsx
+++ src/components/AuthPage/PhoneInput.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-// import TextField from "../Common/Fields/TextField";
+import TextField from "../Common/Fields/TextField";
 import "./hitest.css";
-import { TextField } from "@mui/material";
+import { MdPhoneIphone } from "react-icons/md";
 import { div } from "framer-motion/client";
+import { FaHouse } from "react-icons/fa6";
 const PhoneInput = ({ titleF, PlaceholderF }) => {
   return (
-    <div>
-      <span className="text-xl">{titleF}</span>
-      <div className="flex flex-col w-[538px] bg-primaryGray rounded-3xl text-5xl ">
+    <div className=" max-md:w-[345px] max-md:mx-auto">
+      <span className="text-xl md:mx-12">{titleF}</span>
+      <div className="flex flex-col w-[538px] bg-primaryGray rounded-3xl text-5xl mt-2 md:mx-12  max-md:w-[345px] max-md:items-start ">
         <TextField
-          label={PlaceholderF}
-          InputProps={{
-            className: "hitest",
-          }}
+          placeholder="شماره همراه خود را وارد کنید"
+          icon={MdPhoneIphone}
         />
       </div>
     </div>
