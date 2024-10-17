7c195ad4e8d10303d14af6c47142af90c0b30bef
Aiden <amir138204@yahoo.com>
2024-10-17 21:45:02 +0330

    Revert "Merge branch 'main' of https://github.com/academy-react-summer1403/finalTaskAbSquad into Safetynet4"
    This reverts commit 5602f6de0b076793e593b61b1df8ce5448235574, reversing
    changes made to e8a0a9e70d0cdbd1288ce7d5a3786902d0ca2bbb.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import "./App.css";
 import { PageRoutes } from "../../Routes"; // routes for pages
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
 const router = createBrowserRouter(PageRoutes);
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <RouterProvider router={router}></RouterProvider>
-    </>
+    </Provider>
   );
 }
 
