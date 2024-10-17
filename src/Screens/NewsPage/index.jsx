5cd2ba4e99002419e59ac35f70d7b82ac2545e37
Aiden <amir138204@yahoo.com>
2024-10-17 22:01:15 +0330

    Reapply "Merge branch 'Aiden' of https://github.com/academy-react-summer1403/finalTaskAbSquad"
    This reverts commit a470a8b2be1c40413a0f7d4fc5d4e9de9748b041.

--- src/Screens/NewsPage/index.jsx
+++ src/Screens/NewsPage/index.jsx
@@ -1,9 +1,17 @@
 ﻿import React from "react";
 import NewsApp from "../../components/NewsApp";
+import { motion } from "framer-motion";
 const NewsPage = () => {
   return (
     <>
-      <NewsApp />
+      <motion.div
+        initial={{ x: "-100%" }}
+        animate={{ x: 0 }}
+        exit={{ x: "100%" }}
+        transition={{ duration: 0.1 }}
+      >
+        <NewsApp />
+      </motion.div>
     </>
   );
 };
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,35 @@
 import * as React from "react";
 import "./App.css";
 import { PageRoutes } from "../../Routes"; // routes for pages
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "../../redux/store";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 const router = createBrowserRouter(PageRoutes);
+// An Auxilary Way For Adding Animate Presence
+const Main = () => {
+  const location = useLocation();
+  return (
+    <>
+      <AnimatePresence>
+        <RouterProvider
+          router={router}
+          location={location}
+          key={location.pathname}
+        />
+      </AnimatePresence>
+    </>
+  );
+};
 function App() {
   return (
-    <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
-    </Provider>
+    <>
+      <RouterProvider router={router}>
+        <Main />
+      </RouterProvider>
+    </>
   );
 }
 
