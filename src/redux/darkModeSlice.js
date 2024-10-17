24f60df30c148b3d88526697a7eff95ecd9267f8
Aiden <amir138204@yahoo.com>
2024-10-17 21:29:21 +0330

    Revert "Merge branch 'Sharmande'"
    This reverts commit 5e74a7c5473d2e6a79e604c30f691e6003e8121f, reversing
    changes made to b2d4a98abe98b78cb9a7c49a7085df9bd4705551.

---
+++ src/redux/darkModeSlice.js
@@ -0,0 +1,15 @@
+﻿import { createSlice } from "@reduxjs/toolkit";
+
+const DarkModeSlice = createSlice({
+  name: "darkMode",
+  initialState: {
+    mode: "off",
+  },
+  reducers: {
+    HandleDarkMode: (darkMode, action) => {
+      darkMode.mode = action;
+    },
+  },
+});
+export const { HandleDarkMode } = DarkModeSlice.actions;
+export default DarkModeSlice.reducer;
