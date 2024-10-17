dfb2cde8cd42b0218ba9a040ec6baed7b5ec1957
Aiden <amir138204@yahoo.com>
2024-10-17 21:28:45 +0330

    Reapply "Merge branch 'Sharmande' of https://github.com/academy-react-summer1403/finalTaskAbSquad into Aiden"
    This reverts commit c35d7997029756c8b09820d15d21d4efb8f246ac.

--- src/components/AuthPage/RightSection.jsx
+++ src/components/AuthPage/RightSection.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import BahrLogoComp from "../Common/BahrLogoComp";
 import RightSecRow from "./RightSecRow";
-
+import "./RightSec.css";
 const RightSection = ({ rightSecData }) => {
   return (
-    <div className="bg-primaryGray w-1/3 p-24 h-screen text-primaryWhite">
-      <div className="flex flex-row justify-start items-center max-lg:grow mb-16">
+    <div className="md:bg-primaryGray md:w-1/3 md:p-24 md:h-screen text-primaryWhite max-md:w-full max-md:p-4 max-md:h-auto">
+      <div className="flex flex-row justify-start items-center max-md:hidden mb-16 ">
         <BahrLogoComp />
       </div>
       {rightSecData.map((item, index) => (
         <RightSecRow
           key={`${item.desc}-${index}`}
           desc={item.desc}
+          classNameButton={item.classNameButton}
+          classNameDesc={item.classNameDesc}
           Icon={item.Icon}
         />
       ))}
